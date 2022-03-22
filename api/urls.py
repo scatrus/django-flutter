c48b8c364735add1a8221eee858ec5715ce51251
scatrus <12qwasZXwikb6nms>
2022-03-21 21:58:03 -0300

    update API

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
     path('', views.get_routes),
     path('places', views.get_place),
     path('academys', views.get_academys),
