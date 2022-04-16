898775efc6bbfb248450e84b9c3ca531d6dc6f42
scatrus <12qwasZXwikb6nms>
2022-04-16 16:17:06 -0300

    update API with generator

--- api/views.py
+++ api/views.py
@@ -1,7 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from api.models import Teacher, Student, Academy, Place, Classroom, Group, Presence
-from api.serializers import TeacherSerializer, StudentSerializer, AcademySerializer, PlaceSerializer, ClassroomSerializer, GroupSerializer, PresenceSerializer
+from api.serializers import TeacherSerializer, StudentSerializer, AcademySerializer, PlaceSerializer, \
+    ClassroomSerializer, GroupSerializer, PresenceSerializer
 
 
 @api_view(['GET', 'POST'])
